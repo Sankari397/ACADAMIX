df0de105301eb3d8082302d342a61474d1234004
Sankari S <sankarisk2006@gmail.com>
2026-05-01 10:05:12 +0530

    Fix import paths from './pages/' to './page/'

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,27 +1,21 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import Index from "./pages/Index.tsx";
-import NotFound from "./pages/NotFound.tsx";
+// App.tsx
 
-const queryClient = new QueryClient();
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './page/Home';
+import About from './page/About';
+import Contact from './page/Contact';
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route path='/' component={Home} exact />
+                <Route path='/about' component={About} />
+                <Route path='/contact' component={Contact} />
+            </Switch>
+        </Router>
+    );
+}
 
 export default App;
